9a9045d41d87bb838724e498b47f08e0a72991a0
moahmed amine mezghich <git@mezghich.com>
2023-10-21 15:51:26 +0100

    Add de nouvelle méthode "productByName" dans le repository

--- src/main/java/com/sip/ams/repositories/ProductRepository.java
+++ src/main/java/com/sip/ams/repositories/ProductRepository.java
@@ -2,9 +2,17 @@ package com.sip.ams.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.sip.ams.entities.Product;
 @RepositoryRestResource
 public interface ProductRepository extends JpaRepository<Product , Integer>{
+	
+	 	@Query("select p from Product p where p.name like :name")
+	    public Page<Product> productByName(@Param("name") String mc, Pageable pageable);
+
 
 }
