e87040c6513b7b8b2eb7941405f835bfd1cf599e
moahmed amine mezghich <git@mezghich.com>
2023-10-21 15:31:41 +0100

    service product Ok

--- src/main/java/com/sip/ams/ProviderServiceFullStackApplication.java
+++ src/main/java/com/sip/ams/ProviderServiceFullStackApplication.java
@@ -2,12 +2,28 @@ package com.sip.ams;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.sip.ams.entities.Product;
+import com.sip.ams.repositories.ProductRepository;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
-public class ProviderServiceFullStackApplication {
+public class ProviderServiceFullStackApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProviderServiceFullStackApplication.class, args);
 	}
+
+	@Override
+    public void run(String ... strings) throws Exception {
+        Stream.of("Pencil", "Book", "Eraser").forEach(s->productRepository.save(new Product(s)));
+        productRepository.findAll().forEach(s->System.out.println(s.getName()));
+    }
+
+    @Autowired
+    private ProductRepository productRepository;
 
 }
